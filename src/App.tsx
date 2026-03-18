1bd3868cb4fae1fe7a7b9e15fab222279132fba9
Sathvik Rudam <145160170+sathvikrudam@users.noreply.github.com>
2026-03-18 21:16:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,20 +33,24 @@ function App() {
 }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+
+      <Cursor />   {/* ✅ ADD HERE (top-most UI layer) */}
+
+      <Toaster />
+      <Sonner />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 }
 
 export default App;
