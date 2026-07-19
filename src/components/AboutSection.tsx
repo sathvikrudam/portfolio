8cf02b71c1c2a5e8da03d0a8a544ee9dfe77c085
Sathvik Rudam <145160170+sathvikrudam@users.noreply.github.com>
2026-07-19 16:49:07 +0530

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -13,7 +13,7 @@ const stats = [
   { value: "9.36", label: "CGPA" },
   { value: "5+", label: "Projects" },
   { value: "100+", label: "Problems Solved Across Platforms" },
-  { value: "3+", label: "ProgramminLanguages" },
+  { value: "3+", label: "Programming Languages" },
 ];
 
 const AboutSection = () => {
